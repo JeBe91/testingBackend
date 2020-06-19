b5ecf26a776afaf7394dabd57d4e2cbf16304e55
Jens Bernhart <jens.bernhart@volkswagen.de>
2020-06-19 08:45:55 +0200

    changed ddl-auto philosophie from create to update

--- src/main/java/com/example/sampleProject/controller/StatelessController.java
+++ src/main/java/com/example/sampleProject/controller/StatelessController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @RestController
 @CrossOrigin(origins = { "http://frontend-sample-project-dl-f73-sandbox.apps.ocp3.eu-west-1.stable.aws.vwdl.de/", "http://frontend-sample-project-dl-f73-sandbox.apps.ocp3.eu-west-1.stable.aws.vwdl.de" })
 public class StatelessController {
